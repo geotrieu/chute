7cec6fcb24067e8424b456032a802eae8c4d01b3
Thayallan-s <thayallan.srinathan@gmail.com>
2018-11-03 14:23:03 -0400

    Hotspot card view

--- Chute-ios-application/Chute-ios/Features/Home/HomeViewController.swift
+++ Chute-ios-application/Chute-ios/Features/Home/HomeViewController.swift
@@ -13,8 +13,16 @@ import Then
 class HomeViewController: UIViewController {
     
     let headerView = HomeHeaderView()
-   
+
     let scrollView = HomeStatsScrollView()
+    
+    let hotspotsLabel = UILabel().then {
+        $0.textColor = UI.Colors.blue
+        $0.font = UI.Font.demiBold(30)
+        $0.text = "Hotspots"
+    }
+    
+    let hotspotsView = HomeHotspotsCardView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +43,12 @@ extension HomeViewController {
         
         view.addSubview(scrollView)
         scrollView.easy.layout(Top(5).to(headerView), Left(), Right(), Height(100))
+        
+        view.addSubview(hotspotsLabel)
+        hotspotsLabel.easy.layout(Top(15).to(scrollView), Left(20))
+        
+        view.addSubview(hotspotsView)
+        hotspotsView.easy.layout(Top(20).to(hotspotsLabel), CenterX())
         
     }
 }
